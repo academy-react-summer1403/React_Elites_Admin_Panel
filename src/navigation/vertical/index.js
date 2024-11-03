a016554b991ec846144930fc9b6dac37ebfb14cf
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2024-11-03 21:07:26 +0330

    Initial project

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,64 @@
+import { Mail, Home, Airplay, Circle } from "react-feather";
+
+export default [
+  {
+    id: "dashboard",
+    title: "داشبورد",
+    icon: <Home size={20} />,
+    navLink: "/home",
+  },
+  {
+    id: "courseManagement",
+    title: " مدیریت دوره ",
+    icon: <Mail size={20} />,
+    navLink: "/second-page",
+    children: [
+      {
+        id: "courseManagementList",
+        title: "لیست دوره ها",
+        icon: <Circle size={12} />,
+        navLink: "/apps/invoice/list",
+      },
+      {
+        id: "createNewCourse",
+        title: " دوره جدید ",
+        icon: <Circle size={12} />,
+        navLink: "/apps/invoice/list",
+      },
+    ],
+  },
+  {
+    id: "blogsManagement",
+    title: " مدیریت بلاگ ",
+    icon: <Airplay size={20} />,
+    // navLink: "/sample",
+    children: [
+      {
+        id: "blogsManagementList",
+        title: " لیست بلاگ ها ",
+        icon: <Circle size={12} />,
+        navLink: "/apps/invoice/list",
+      },
+      {
+        id: "createNewBlog",
+        title: " بلاگ جدید ",
+        icon: <Circle size={12} />,
+        navLink: "/apps/invoice/list",
+      },
+    ],
+  },
+  {
+    id: "userManagement",
+    title: " مدیریت کاربر ها ",
+    icon: <Mail size={20} />,
+    navLink: "/second-page",
+    children: [
+      {
+        id: "usersManagementList",
+        title: " لیست کاربر ها ",
+        icon: <Circle size={12} />,
+        navLink: "/apps/invoice/list",
+      },
+    ],
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardLink,
+} from "reactstrap";
+import CardMedal from "../@core/components/Card Medal/CardMedal";
+
+const Home = () => {
+  return (
+    <div>
+      <CardMedal />
+    </div>
+  );
+};
+
+export default Home;
